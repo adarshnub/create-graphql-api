d8076dd6d5410e3bf06a0866c80532764976e7f7
adarshnub <adarshmanjady@gmail.com>
2023-02-02 10:45:40 +0530

    final commit

--- app/app.js
+++ app/app.js
@@ -4,26 +4,19 @@ import http from "http";
 const bodyParser = require('body-parser')
 const { ApolloServer } = require('apollo-server-express')
 const cors = require('cors')
+
+
+//creates the express application
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-// const server = new ApolloServer({
-//     modules: [
-//         require('./GraphQL/tickets'),
-//         require('./GraphQL/status'),
-//         require('./GraphQL/users'),
-//         require('./GraphQL/priorities'),
-//     ],
-// })
 
-// const text = server.applyMiddleware({ app })
-// console.log(text);
- 
-let apolloServer = null;
+ //create apollo server instance
+let apolloServer = null; //
 async function startServer() {
-    apolloServer = new ApolloServer({
+    apolloServer = new ApolloServer({  //create new instance of apollo server
         modules: [
             require('./GraphQL/tickets'),
             require('./GraphQL/status'),
@@ -31,7 +24,8 @@ async function startServer() {
             require('./GraphQL/priorities'),
         ],
     })
-    await apolloServer.start();
+    await apolloServer.start();       //start apollo Server
+    //apply Apollo middleware to express app
     apolloServer.applyMiddleware({ app,
         path: '/',
         cors: true,
@@ -41,7 +35,7 @@ startServer();
 const httpserver = http.createServer(app);
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+//express server startup
 app.listen({ port: 5000 }, () =>
     console.log(`🚀 Server ready at http://localhost:5000`),
 )
